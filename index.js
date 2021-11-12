257095bdb8dc1b8f3c9871125d429fe1fb0031cb
ikiqqq <riskidarliana76@gmail.com>
2021-11-12 20:31:25 +0700

    + sort

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 function anagram (word1, word2){
-    word1 = word1.toLowerCase("").split("").join("").trim("")
-    word2 = word2.toLowerCase("").split("").join("").trim("")
+    word1 = word1.toLowerCase("").split("").sort().join("").trim("")
+    word2 = word2.toLowerCase("").split("").sort().join("").trim("")
     
     return word1 === word2
     
 }
 
+console.log (anagram("tets","te"))
 console.log (anagram("tets","stet"))
 
